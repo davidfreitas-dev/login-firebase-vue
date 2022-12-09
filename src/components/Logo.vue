ac0708e337684d1ab62f3c6df01a084298d4779b
David Freitas <davidfreitas.dev@outlook.com>
2022-12-09 07:16:15 -0300

    Ajuste centralizacao do formulario de login, registro e recuperacao de senha

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-row justify-center items-center">
-    <img src="../assets/vue.svg" alt="Logo Vue JS" class="ml-7 w-24 h-24">
+    <img src="../assets/vue.svg" alt="Logo Vue JS" class="ml-7 w-20 h-20">
     <span class="ml-5 text-3xl text-white"> + </span>
-    <img src="../assets/firebase.svg" alt="Logo Vue JS" class="w-36 h-36">
+    <img src="../assets/firebase.svg" alt="Logo Vue JS" class="w-32 h-32">
   </div>
 </template>
